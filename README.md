2602a9a4af0189856ff5c4650134f27de1dcddd1
brit200313 <brittany.jill.martin@gmail.com>
2014-02-22 19:51:37 +0000

    First commit

---
+++ README.md
@@ -0,0 +1,6 @@
+
+#WordtoPix Backend
+
+Built the backend of the gaming app we built at a hackathon
+
+www.wordtopix.co
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wordtopixback::Application.config.secret_token = 'c7dd11e086b5df26db8e557b0be462f7e667cb83cb8b26504a1b560431f70a435d0b80aa95ee8b3c446795e25fda038adfc84a8922a925f1c7deb55f93e9f65a'
